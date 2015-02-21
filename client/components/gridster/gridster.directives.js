04214ebb8666a64a9bb6fedbe931d861c5bbc87e
Brandon Morrison <brmorrison61@gmail.com>
2015-02-20 23:39:50 -0800

    gridster instantiation from link in directive

--- client/components/gridster/gridster.directives.js
+++ client/components/gridster/gridster.directives.js
@@ -18,6 +18,10 @@ angular.module('appApp')
 	            scope.createRow = function(){
 					console.log(element);
 					element.find('ul').append('<li data-row="1" data-col="1" data-sizex="1" data-sizey="1"></li><li data-row="2" data-col="1" data-sizex="1" data-sizey="1"></li><li data-row="3" data-col="1" data-sizex="1" data-sizey="1"></li><li data-row="1" data-col="2" data-sizex="2" data-sizey="1"></li><li data-row="2" data-col="2" data-sizex="2" data-sizey="2"></li><li data-row="1" data-col="4" data-sizex="1" data-sizey="1"></li><li data-row="2" data-col="4" data-sizex="2" data-sizey="1"></li><li data-row="3" data-col="4" data-sizex="1" data-sizey="1"></li><li data-row="1" data-col="5" data-sizex="1" data-sizey="1"></li><li data-row="3" data-col="5" data-sizex="1" data-sizey="1"></li><li data-row="1" data-col="6" data-sizex="1" data-sizey="1"></li><li data-row="2" data-col="6" data-sizex="1" data-sizey="2"></li>');
+					$(".gridster ul").gridster({
+				        widget_margins: [10, 10],
+				        widget_base_dimensions: [140, 140]
+				    });
 				} 
 	        },
 			//templateUrl: 'edit-avatar.html'
